6b1486106eaff90fcdde48e602b81f316a4bd488
Leo-zang000 <leonardozang565@gmail.com>
2026-01-06 19:34:25 +0000

    built out the gameplay loop

--- board.rb
+++ board.rb
@@ -4,11 +4,19 @@ class Board
   def initialize()
     @dimensions = 3
     @positions_filled = 0
-    @positions_matrix = Array.new(3, Array.new(3, "*"))
+    @positions_matrix = Array.new(3){Array.new(3, "*")}
   end
+  
   def add_counter(counter)
-    @positions_matrix[counter.y_pos?][counter.x_pos?] = counter.type?
+    if @positions_matrix[counter.y_pos?][counter.x_pos?] == "*"
+      @positions_matrix[counter.y_pos?][counter.x_pos?] = counter.type?
+      @positions_filled += 1
+    else
+      return true
+    end
+    return false
   end
+
   def display()
     puts ("   1  2  3")
     0.upto(2) do |line_num|
@@ -18,5 +26,72 @@ class Board
       end
       print "\n"
     end
+  end
+  def positions_filled?()
+    return @positions_filled
+  end
+  def winning_position?()
+    won = false
+    player_who_won = "game ongoing"
+    if @positions_matrix[0][0] != "*" and @positions_matrix[0][0] == @positions_matrix[0][1] and @positions_matrix[0][1] == @positions_matrix[0][2]
+      won = true
+      if @positions_matrix[0][0] == "X"
+          player_who_won = "Player 1"
+      else
+        player_who_won = "Player 2"
+      end 
+    elsif @positions_matrix[1][0] != "*" and @positions_matrix[1][0] == @positions_matrix[1][1] and @positions_matrix[1][1] == @positions_matrix[1][2]
+      won = true
+      if @positions_matrix[1][0] == "X"
+          player_who_won = "Player 1"
+      else
+        player_who_won = "Player 2"
+      end 
+    elsif @positions_matrix[2][0] != "*" and  @positions_matrix[2][0] == @positions_matrix[2][1] and @positions_matrix[2][1] == @positions_matrix[2][2]
+      won = true
+      if @positions_matrix[2][0] == "X"
+          player_who_won = "Player 1"
+      else
+        player_who_won = "Player 2"
+      end 
+    elsif @positions_matrix[0][0] != "*" and @positions_matrix[0][0] == @positions_matrix[1][0] and @positions_matrix[1][0] == @positions_matrix[2][0]
+      won = true
+      if @positions_matrix[0][0] == "X"
+          player_who_won = "Player 1"
+      else
+        player_who_won = "Player 2"
+      end 
+    elsif @positions_matrix[0][1] != "*" and @positions_matrix[0][1] == @positions_matrix[1][1] and @positions_matrix[1][1] == @positions_matrix[2][1]
+      won = true
+      if @positions_matrix[0][1] == "X"
+          player_who_won = "Player 1"
+      else
+        player_who_won = "Player 2"
+      end 
+    elsif @positions_matrix[0][2] != "*" and @positions_matrix[0][2] == @positions_matrix[1][2] and @positions_matrix[1][2] == @positions_matrix[2][2]
+      won = true
+      if @positions_matrix[0][2] == "X"
+          player_who_won = "Player 1"
+      else
+        player_who_won = "Player 2"
+      end 
+    elsif @positions_matrix[0][0] != "*" and @positions_matrix[0][0] == @positions_matrix[1][1] and @positions_matrix[1][1] == @positions_matrix[2][2]
+      won = true
+      if @positions_matrix[0][0] == "X"
+          player_who_won = "Player 1"
+      else
+        player_who_won = "Player 2"
+      end 
+    elsif @positions_matrix[0][2] != "*" and @positions_matrix[0][2] == @positions_matrix[1][1] and @positions_matrix[1][1] == @positions_matrix[2][0]
+      won = true
+      if @positions_matrix[0][0] == "X"
+          player_who_won = "Player 1"
+      else
+        player_who_won = "Player 2"
+      end
+    else
+      won = false
+    end
+    return player_who_won, won
   end
 end
--- main.rb
+++ main.rb
@@ -3,32 +3,50 @@ require_relative("board.rb")
 require_relative("counter.rb")
 
 def player_move(player, board)
-  puts(player +"'s move:")
-  print("Input the column number that you would like to place the counter in:")
-  counter_x_coordinate = gets.chomp
-  print("Input the row number that you would like to place the counter in:")
-  counter_y_coordinate = gets.chomp
-  counter_type = ""
-  if player == "Player 1"
-    counter_type = "X"
-  else
-    counter_type = "O"
+  puts(player + "'s move:")
+  repeat_until_valid = true
+  while repeat_until_valid
+    print("Input the column number that you would like to place the counter in:")
+    counter_x_coordinate = gets.chomp.to_i - 1
+    print("Input the row number that you would like to place the counter in:")
+    counter_y_coordinate = gets.chomp.to_i - 1
+    counter_type = ""
+    if player == "Player 1"
+      counter_type = "X"
+    else
+      counter_type = "O"
+    end
+    current_counter = Counter.new(counter_type, counter_x_coordinate, counter_y_coordinate)
+    repeat_until_valid = board.add_counter(current_counter)
+    board.display()
   end
-  current_counter = Counter.new(counter_type, counter_x_coordinate, counter_y_coordinate)
-  board.add_counter()
 end
 
 game_looping = true
-while game_looping
   current_board = Board.new()
   current_board.display()
+while game_looping
 
   player_move("Player 1", current_board)
-  player_move("Player 2", current_board)
 
-  if current_board.positions_filled >= 9
-    game_looping = false
-    print("Board filled - game over - draw")
+  if current_board.winning_position?[1] == true
+      game_looping = false
+      puts(current_board.winning_position?[0] + " has won the game!")
+  elsif current_board.positions_filled? >= 9
+      game_looping = false
+      puts("Board filled - game over - draw")
+  end
+
+  if game_looping == true
+    player_move("Player 2", current_board)
+
+    if current_board.winning_position?[1] == true
+        game_looping = false
+        puts(current_board.winning_position?[0] + " has won the game!")
+    elsif current_board.positions_filled? >= 9
+        game_looping = false
+        puts("Board filled - game over - draw")
+    end
   end
 
 end
